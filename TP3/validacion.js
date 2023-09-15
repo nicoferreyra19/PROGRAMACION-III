041b85a105dbdc2277b71d0d4ea9ea75891afa80
Fantitom03 <leivafrancisco2017@gmail.com>
2023-09-15 12:11:40 -0300

    Registro PTF con validación de fechas

---
+++ TP3/validacion.js
@@ -0,0 +1,12 @@
+const formulario1 = document.getElementById("formulario_PTF");
+
+formulario1.addEventListener("submit", function(e){
+   let inputFecha = document.getElementById("fecha_presentacion").value;
+   let fechaActual = new Date();
+   let fechaForm = new Date(inputFecha);
+
+   if(fechaForm > fechaActual){
+      e.preventDefault();
+      alert('Error. La fecha es posterior a la actual');
+   }
+});
